0216f80c21a6f4b7d9d51a3ecb67a39be366673d
AmplifiedHuman <amplifiedhuman@gmail.com>
2022-04-25 12:34:28 +0100

    added firewall implementation

---
+++ firewall/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from numpy import source
+from scapy.all import *
+from scapy.sendrecv import sendp, sniff
+import sys
+
+source_interface = ''
+destination_interface = ''
+
+
+def send_packets(packet):
+    sendp(packet, destination_interface)
+
+
+def start():
+    sniff(iface=source_interface, prn=send_packets)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(
+            "Usage: python3 main.py [source_interface] [destination_interface]")
+    else:
+        source_interface = sys.argv[1]
+        destination_interface = sys.argv[2]
+        start()
