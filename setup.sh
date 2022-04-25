2eaa5bc16b92154783cfb63e13e2e8231868b97e
AmplifiedHuman <amplifiedhuman@gmail.com>
2022-04-25 19:00:59 +0100

    add snort container to setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ ovs-docker del-port ovs-br1 eth1 client
 ovs-docker add-port ovs-br1 eth1 client --ipaddress=192.168.0.2/24
 ovs-docker del-port ovs-br1 eth1 firewall
 ovs-docker add-port ovs-br1 eth1 firewall --ipaddress=192.168.0.3/24
+ovs-docker del-port ovs-br2 eth2 firewall
+ovs-docker add-port ovs-br2 eth2 firewall --ipaddress=192.168.1.2/24
+ovs-docker del-port ovs-br2 eth2 snort
+ovs-docker add-port ovs-br2 eth2 snort --ipaddress=192.168.1.3/24
