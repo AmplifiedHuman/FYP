5c3e0783a66cd474d5b68f130ee90182d20c71c5
AmplifiedHuman <amplifiedhuman@gmail.com>
2022-04-26 23:57:28 +0100

    rewire sfc chain

--- firewall/firewall_setup.sh
+++ firewall/firewall_setup.sh
@@ -2,5 +2,5 @@ ip link set eth1 promisc on
 ethtool -K eth1 gro off lro off
 ip link set eth2 promisc on
 ethtool -K eth2 gro off lro off
-ip link set eth4 promisc on
-ethtool -K eth4 gro off lro off
+ip link set eth3 promisc on
+ethtool -K eth3 gro off lro off
