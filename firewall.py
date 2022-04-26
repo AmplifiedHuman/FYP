56d24d75d72e7946a9d1876f755e3afb5ffc6abd
AmplifiedHuman <amplifiedhuman@gmail.com>
2022-04-26 23:30:34 +0100

    added firewall

---
+++ firewall.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from scapy.all import *
+from scapy.sendrecv import sendp, sniff
+import sys
+
+source_interface = ''
+normal_destination = ''
+icmp_destination = ''
+filter_function = None
+
+
+def send_packets(packet):
+    if ICMP in packet:
+        sendp(packet, icmp_destination)
+    else:
+        sendp(packet, normal_destination)
+
+
+def start():
+    sniff(iface=source_interface, prn=send_packets)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(
+            "Usage: python3 route.py <source_interface> <normal_destination> <icmp_destination>")
+    else:
+        source_interface = sys.argv[1]
+        normal_destination = sys.argv[2]
+        icmp_destination = sys.argv[3]
+        start()
