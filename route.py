233645eb5d6e360812bd780ea420b8c0e0b7ad3c
AmplifiedHuman <amplifiedhuman@gmail.com>
2022-04-26 05:51:53 +0100

    minor rename

--- route.py
+++ route.py
@@ -7,7 +7,7 @@ import sys
 
 source_interface = ''
 destination_interface = ''
-
+filter_function = None
 
 def send_packets(packet):
     sendp(packet, destination_interface)
@@ -16,11 +16,10 @@ def send_packets(packet):
 def start():
     sniff(iface=source_interface, prn=send_packets)
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(
-            "Usage: python3 main.py [source_interface] [destination_interface]")
+            "Usage: python3 route.py <source_interface> <destination_interface>")
     else:
         source_interface = sys.argv[1]
         destination_interface = sys.argv[2]
